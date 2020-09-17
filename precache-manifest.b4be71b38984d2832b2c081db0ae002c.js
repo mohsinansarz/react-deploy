386a415d8d1cd88083062b18da76531eb41f6096
mohsinansarz <32729135+mohsinansarz@users.noreply.github.com>
2020-09-17 11:26:57 +0530

    Updates

---
+++ precache-manifest.b4be71b38984d2832b2c081db0ae002c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ac869b07fa95608cd4953428761812",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "4898a02b85a97a8c735c",
+    "url": "/react-deploy/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "dcb163bd4bf6f8d11458",
+    "url": "/react-deploy/static/css/main.3bde81bd.chunk.css"
+  },
+  {
+    "revision": "4898a02b85a97a8c735c",
+    "url": "/react-deploy/static/js/2.20c75e1e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-deploy/static/js/2.20c75e1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcb163bd4bf6f8d11458",
+    "url": "/react-deploy/static/js/main.6b2dcd9c.chunk.js"
+  },
+  {
+    "revision": "466be2985b146550d3c3",
+    "url": "/react-deploy/static/js/runtime-main.f81fbcab.js"
+  }
+]);
